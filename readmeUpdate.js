bc4866d21f03c4146ff35fe0683d2f48d4bad7d1
bbakseong <psm651@naver.com>
2024-02-13 21:32:59 +0900

    update github

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -7,45 +7,54 @@ import Parser from "rss-parser";
  */
 let text = `# Hi there 👋
 
-   ## 이런 환경에 익숙해요✍🏼
+[![Anurag's GitHub stats](https://github-readme-stats.vercel.app/api?username=psm651)](https://github.com/anuraghazra/github-readme-stats)
+[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=psm651&layout=compact&hide=r,jupyter%20notebook,c%23&exclude_repo=roharui.github.io)](https://github.com/anuraghazra/github-readme-stats)
 
-   ## 언어
+## 이런 환경에 익숙해요✍🏼
 
-   <p>
-	 <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/>
-	   <img alt="" src= "https://img.shields.io/badge/TypeScript-black?logo=typescript&logoColor=blue"/>
-	   </p>
+## 언어
 
-	   ## Contact me
+<p>
+  <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/> 
+  <img alt="" src= "https://img.shields.io/badge/TypeScript-black?logo=typescript&logoColor=blue"/>
+</p>
 
-	   ## 📕 Latest Blog Posts
+## Contact me
 
-	   `;
+psm651@naver.com
+
+## 📕 Latest Blog Posts
+<p>
+    <a href="https://systorage.tistory.com/"><img src="https://img.shields.io/badge/Blog-FF5722?style=flat-square&logo=Blogger&logoColor=white"/></a><br>
+</p>
+
+`;
 
 // rss-parser 생성
 const parser = new Parser({
-	headers: {
-		Accept: 'application/rss+xml, application/xml, text/xml; q=0.1',
-	}});
+    headers: {
+        Accept: 'application/rss+xml, application/xml, text/xml; q=0.1',
+    }});
 
 (async () => {
 
-	// 피드 목록
-	const feed = await parser.parseURL('https://shutcoding.tistory.com/rss');
+    // 피드 목록
+    const feed = await parser.parseURL('https://shutcoding.tistory.com/rss');
 
-	// 최신 5개의 글의 제목과 링크를 가져온 후 text에 추가
-	for (let i = 0; i < 5; i++) {
-		const {title, link} = feed.items[i];
-		console.log(`${i + 1}번째 게시물`);
-		console.log(`추가될 제목: ${title}`);
-		console.log(`추가될 링크: ${link}`);
-		text += `<a href=${link}>${title}</a></br>`;
-	}
+    // 최신 5개의 글의 제목과 링크를 가져온 후 text에 추가
+    for (let i = 0; i < 5; i++) {
+        const {title, link} = feed.items[i];
+        console.log(`${i + 1}번째 게시물`);
+        console.log(`추가될 제목: ${title}`);
+        console.log(`추가될 링크: ${link}`);
+        text += `<a href=${link}>${title}</a></br>`;
+    }
 
-	// README.md 파일 작성
-	writeFileSync('README.md', text, 'utf8', (e) => {
-		console.log(e)
-	})
+    // README.md 파일 작성
+    writeFileSync('README.md', text, 'utf8', (e) => {
+        console.log(e)
+    })
 
-	console.log('업데이트 완료')
+    console.log('업데이트 완료')
 })();
+
